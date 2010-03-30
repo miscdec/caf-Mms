b10ba3b0853b3c589214f5ec55fe81ffcbe18676
Naveen Kalla <nkalla@codeaurora.org>
2010-03-29 18:45:19 -0700

    Display message when operation fails since FDN being enabled
    The terminal disallows SMS operations for numbers which are not in the Fixed
    Dialing Number (FDN) list when the FDN service is enabled. FDN_CHECK_FAILURE
    error is sent from by RIL when this happens. Display a message to the user.
    
    Change-Id: Iee173cef9e443edafd4bcad2d239518dbdf8e598

--- src/com/android/mms/transaction/SmsReceiverService.java
+++ src/com/android/mms/transaction/SmsReceiverService.java
@@ -90,7 +90,13 @@ public class SmsReceiverService extends Service {
 
     };
 
-    public Handler mToastHandler = new Handler();
+    public Handler mToastHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(SmsReceiverService.this, getString(msg.what),
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
 
     // This must match SEND_PROJECTION.
     private static final int SEND_COLUMN_ID         = 0;
@@ -277,12 +283,9 @@ public class SmsReceiverService extends Service {
             registerForServiceStateChanges();
             // We couldn't send the message, put in the queue to retry later.
             Sms.moveMessageToFolder(this, uri, Sms.MESSAGE_TYPE_QUEUED, error);
-            mToastHandler.post(new Runnable() {
-                public void run() {
-                    Toast.makeText(SmsReceiverService.this, getString(R.string.message_queued),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
+            mToastHandler.sendEmptyMessage(R.string.message_queued);
+        } else if (mResultCode == SmsManager.RESULT_ERROR_FDN_CHECK_FAILURE) {
+            mToastHandler.sendEmptyMessage(R.string.fdn_check_failure);
         } else {
             if (Log.isLoggable(LogTag.TRANSACTION, Log.VERBOSE)) {
                 Log.v(TAG, "handleSmsSent msg failed uri: " + uri);
