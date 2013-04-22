ea227670622780abbc0a55dad002ff6dfb045f2b
kaiyiz <kaiyiz@codeaurora.org>
2013-04-22 04:34:26 -0400

    Mms: Monkey issue Unable to find or allocate a thread ID
    Method parseMmsAddress() may return an empty string.
    
    Method isValidMmsAddress() return true only retVal is not
    null and is not an empty string.
    
    Change-Id: I1bd017965e080f9c73474db4efa9c8b10eacb6fa

--- src/com/android/mms/ui/MessageUtils.java
+++ src/com/android/mms/ui/MessageUtils.java
@@ -987,7 +987,7 @@ public class MessageUtils {
      */
     public static boolean isValidMmsAddress(String address) {
         String retVal = parseMmsAddress(address);
-        return (retVal != null);
+        return (retVal != null && !retVal.equals(""));
     }
 
     /**
