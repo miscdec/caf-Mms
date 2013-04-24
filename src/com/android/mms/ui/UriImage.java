0e9bccb3474791a634408380bcf4ab493c411b25
huangzengzhi <huangzengzhi@hisensecom.com>
2013-04-24 17:51:09 +0800

    fix bug:sms crashed after takeing picture for many times

--- src/com/android/mms/ui/UriImage.java
+++ src/com/android/mms/ui/UriImage.java
@@ -233,8 +233,7 @@ public class UriImage {
     public PduPart getResizedImageAsPart(int widthLimit, int heightLimit, int byteLimit) {
         PduPart part = new PduPart();
 
-        byte[] data =  getResizedImageData(mWidth, mHeight,
-                widthLimit, heightLimit, byteLimit, mUri, mContext);
+        byte[] data = getResizedImageData(widthLimit, heightLimit, byteLimit);
         if (data == null) {
             if (LOCAL_LOGV) {
                 Log.v(TAG, "Resize image failed.");
@@ -245,6 +244,13 @@ public class UriImage {
         part.setData(data);
         // getResizedImageData ALWAYS compresses to JPEG, regardless of the original content type
         part.setContentType(getContentType().getBytes());
+        String src = getSrc();
+        byte[] srcBytes = src.getBytes();
+        part.setContentLocation(srcBytes);
+        part.setFilename(srcBytes);
+        int period = src.lastIndexOf(".");
+        byte[] contentId = period != -1 ? src.substring(0, period).getBytes() : srcBytes;
+        part.setContentId(contentId);
 
         return part;
     }
@@ -400,6 +406,108 @@ public class UriImage {
         } catch (java.lang.OutOfMemoryError e) {
             Log.e(TAG, e.getMessage(), e);
             return null;
+        }
+    }
+
+    private byte[] getResizedImageData(int widthLimit, int heightLimit, int byteLimit) {
+        int outWidth = mWidth;
+        int outHeight = mHeight;
+
+        int scaleFactor = 1;
+        while ((outWidth / scaleFactor > widthLimit) || (outHeight / scaleFactor > heightLimit)) {
+            scaleFactor *= 2;
+        }
+
+        if (LOCAL_LOGV) {
+            Log.v(TAG, "getResizedImageData: wlimit=" + widthLimit +
+                    ", hlimit=" + heightLimit + ", sizeLimit=" + byteLimit +
+                    ", mWidth=" + mWidth + ", mHeight=" + mHeight +
+                    ", initialScaleFactor=" + scaleFactor);
+        }
+
+        InputStream input = null;
+        try {
+            ByteArrayOutputStream os = null;
+            int attempts = 1;
+
+            do {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = scaleFactor;
+                input = mContext.getContentResolver().openInputStream(mUri);
+                int quality = MessageUtils.IMAGE_COMPRESSION_QUALITY;
+                try {
+                    Bitmap b = BitmapFactory.decodeStream(input, null, options);
+                    if (b == null) {
+                        return null;
+                    }
+                    if (options.outWidth > widthLimit || options.outHeight > heightLimit) {
+                        // The decoder does not support the inSampleSize option.
+                        // Scale the bitmap using Bitmap library.
+                        int scaledWidth = outWidth / scaleFactor;
+                        int scaledHeight = outHeight / scaleFactor;
+
+                        if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+                            Log.v(TAG, "getResizedImageData: retry scaling using " +
+                                    "Bitmap.createScaledBitmap: w=" + scaledWidth +
+                                    ", h=" + scaledHeight);
+                        }
+
+                        b = Bitmap.createScaledBitmap(b, outWidth / scaleFactor,
+                                outHeight / scaleFactor, false);
+                        if (b == null) {
+                            return null;
+                        }
+                    }
+
+                    // Compress the image into a JPG. Start with MessageUtils.IMAGE_COMPRESSION_QUALITY.
+                    // In case that the image byte size is still too large reduce the quality in
+                    // proportion to the desired byte size. Should the quality fall below
+                    // MINIMUM_IMAGE_COMPRESSION_QUALITY skip a compression attempt and we will enter
+                    // the next round with a smaller image to start with.
+                    os = new ByteArrayOutputStream();
+                    b.compress(CompressFormat.JPEG, quality, os);
+                    int jpgFileSize = os.size();
+                    if (jpgFileSize > byteLimit) {
+                        int reducedQuality = quality * byteLimit / jpgFileSize;
+                        if (reducedQuality >= MessageUtils.MINIMUM_IMAGE_COMPRESSION_QUALITY) {
+                            quality = reducedQuality;
+
+                            if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+                                Log.v(TAG, "getResizedImageData: compress(2) w/ quality=" + quality);
+                            }
+
+                            os = new ByteArrayOutputStream();
+                            b.compress(CompressFormat.JPEG, quality, os);
+                        }
+                    }
+                } catch (java.lang.OutOfMemoryError e) {
+                    Log.e(TAG, e.getMessage(), e);
+                    // fall through and keep trying with a smaller scale factor.
+                }
+                if (LOCAL_LOGV) {
+                    Log.v(TAG, "attempt=" + attempts
+                            + " size=" + (os == null ? 0 : os.size())
+                            + " width=" + outWidth / scaleFactor
+                            + " height=" + outHeight / scaleFactor
+                            + " scaleFactor=" + scaleFactor
+                            + " quality=" + quality);
+                }
+                scaleFactor *= 2;
+                attempts++;
+            } while ((os == null || os.size() > byteLimit) && attempts < NUMBER_OF_RESIZE_ATTEMPTS);
+
+            return os == null ? null : os.toByteArray();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, e.getMessage(), e);
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
         }
     }
 }
