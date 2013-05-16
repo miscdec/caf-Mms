76d61a611d30fc74644dafb6bf11f920da147417
huangzengzhi <huangzengzhi@hisensecom.com>
2013-05-16 19:16:58 +0800

    share .txt format file to sms,give tips when play slide

--- src/com/android/mms/model/SlideshowModel.java
+++ src/com/android/mms/model/SlideshowModel.java
@@ -111,6 +111,18 @@ public class SlideshowModel extends Model
             Context context, Uri uri) throws MmsException {
         return createFromPduBody(context, getPduBody(context, uri));
     }
+    public boolean isOnlySimpleAttach() {  
+        // There must be one (and only one) slide.
+        if (size() != 1)
+            return false;       
+        SlideModel slide = get(0);
+        if (slide.hasImage() || slide.hasVideo() || slide.hasAudio()
+            || slide.hasText())
+        {
+            return false;
+        }        
+        return true;
+    }
 
     public static SlideshowModel createFromPduBody(Context context, PduBody pb) throws MmsException {
         SMILDocument document = SmilHelper.getDocument(pb);
