a0345bcd7f541db77d1a985a2938167719895675
Suresh Kumar Sugguna <sureshk@codeaurora.org>
2013-05-17 17:08:43 +0530

    Mms: Do not call stopUsingNetworkFeature when another MMS is in process
    Around the time when a big MMS transaction (send or received) is about
    to complete, a new MMS transaction (send or received) is started.
    Due to completion of the previous transation, present code logic
    removes the route by calling stopUsingNetworkFeature(). This
    results in socket failure for the new MMS transaction.
    
    Change-Id: Icec7a662a420f6872f37a24abc8337d4672735ab
    CRs-Fixed: 488519

--- src/com/android/mms/transaction/TransactionService.java
+++ src/com/android/mms/transaction/TransactionService.java
@@ -559,11 +559,16 @@ public class TransactionService extends Service implements Observer {
                             transaction.getConnectionSettings());
                     mServiceHandler.sendMessage(msg);
                 }
-                else {
+                else if (mProcessing.isEmpty()) {
                     if (Log.isLoggable(LogTag.TRANSACTION, Log.VERBOSE)) {
                         Log.v(TAG, "update: endMmsConnectivity");
                     }
                     endMmsConnectivity();
+                }
+                else {
+                    if (Log.isLoggable(LogTag.TRANSACTION, Log.VERBOSE)) {
+                        Log.v(TAG, "update: mProcessing is not empty");
+                    }
                 }
             }
 
@@ -622,7 +627,7 @@ public class TransactionService extends Service implements Observer {
             transaction.detach(this);
             MmsSystemEventReceiver.unRegisterForConnectionStateChanges(getApplicationContext());
             removeNotification(serviceId);
-            stopSelf(serviceId);
+            stopSelfIfIdle(serviceId);
         }
     }
 
