cbdcb62d9694126ebbf5bc0661cc83a81b54d12a
kaiyiz <kaiyiz@codeaurora.org>
2013-11-27 18:22:29 +0800

    Mms: Fix MMS crash when share many video clips via MMS
    The slideshow will not add as attachment if the attachment
    size is larger than the limit size. So slideshow in working
    message is null and it will cause NullPointerException.
    
    Do null check before resize slideshow to avoid exception.
    
    CRs-Fixed: 582337
    
    Change-Id: I3c5e4f538d921be6797cc7a5009fcb4c7c3d2b89

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4532,7 +4532,7 @@ public class ComposeMessageActivity extends Activity
                         Parcelable uri = uris.get(i);
                         addAttachment(mimeType, (Uri) uri, true);
                     }
-                    if (SHOW_SEND_CONFIRM) {
+                    if (SHOW_SEND_CONFIRM && mWorkingMessage.getSlideshow() != null) {
                         mWorkingMessage.getSlideshow().resizeBeforeSendMms();
                     }
                 }
