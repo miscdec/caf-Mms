cc08b8e4aff07dee6119a5c922be2c72e71f20c2
kayiz <kaiyiz@codeaurora.org>
2013-12-12 18:42:14 +0800

    Mms: Fix Mms used display wrong when check memory status
    The app only check the size of database of mms, it didn't add
    the attachment of mms, so the mms used will display wrong.
    
    Use all mms data's size to show mms used.
    
    CRs-fixed: 588656
    
    Change-Id: Iac2ab0ac186fd4384c102750ba907bb65c602060

--- src/com/android/mms/ui/MessageUtils.java
+++ src/com/android/mms/ui/MessageUtils.java
@@ -1661,7 +1661,6 @@ public class MessageUtils {
         long mmsMaxSize = dbSize;
         long mmsMinSize = mmsCount * 3 * 1024;
         long smsSize = smsCount * 1024;
-        mmsfileSize = mmsMaxSize - smsSize;
         return (mmsfileSize < mmsMinSize ? mmsMinSize : mmsfileSize);
     }
 
