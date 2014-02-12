dc0bc878001e05b8a15bbb4fc2d6be5b4880ba62
Zhang Fang <zfang@codeaurora.org>
2014-02-12 19:52:40 +0800

    MMS: Fix issue that MMS can be added in to 301K
    Re-calulate the remain message size with inpurt text size.
    
    CRs-Fixed: 599891
    
    Change-Id: Ib934e0cd4c7550794ddddd549495d00d4aad0e10

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -227,7 +227,7 @@ public class ComposeMessageActivity extends Activity
     private static final int MENU_VIDEOCALL_RECIPIENT   = 8;
     private static final int MENU_SEND_BY_SLOT1         = 9;
     private static final int MENU_SEND_BY_SLOT2         = 10;
-    private static final int MENU_FOWARD_CONVERSATION    = 11;
+    private static final int MENU_FORWARD_CONVERSATION    = 11;
 
     // Context menu ID
     private static final int MENU_VIEW_CONTACT          = 12;
@@ -454,6 +454,7 @@ public class ComposeMessageActivity extends Activity
     public final static String THREAD_ID = "thread_id";
     private final static String RECIPIENTS = "recipients";
     public final static String MANAGE_MODE = "manage_mode";
+    private final static String MSG_SUBJECT_SIZE = "subject_size";
 
     private boolean isLocked = false;
 
@@ -531,7 +532,10 @@ public class ComposeMessageActivity extends Activity
         // The SlideShow is not support Vcard attachment, if we have created a
         // Vcard already before adding SlideShow, we must remove it first.
         SlideshowModel slideShow = mWorkingMessage.getSlideshow();
+        final int subjectSize = mWorkingMessage.hasSubject()
+                    ? mWorkingMessage.getSubject().toString().getBytes().length : 0;
         if (slideShow != null) {
+            slideShow.setSubjectSize(subjectSize);
             for (SlideModel model : slideShow) {
                 if (model != null && model.hasVcard()) {
                     model.removeVcard();
@@ -561,6 +565,7 @@ public class ComposeMessageActivity extends Activity
                 Intent intent = new Intent(ComposeMessageActivity.this,
                         SlideshowEditActivity.class);
                 intent.setData(mTempMmsUri);
+                intent.putExtra(MSG_SUBJECT_SIZE, subjectSize);
                 startActivityForResult(intent, REQUEST_CODE_CREATE_SLIDESHOW);
             }
         }, R.string.building_slideshow_title);
@@ -716,8 +721,14 @@ public class ComposeMessageActivity extends Activity
         if (cursor == null) {
             return false;
         }
+        int subjectSize = (msgItem.mSubject == null) ? 0 : msgItem.mSubject.getBytes().length;
+        int messageSize =  msgItem.mMessageSize + subjectSize;
+        if (DEBUG) {
+            Log.v(TAG,"showMessageDetails subjectSize = " + subjectSize);
+            Log.v(TAG,"showMessageDetails messageSize = " + messageSize);
+        }
         String messageDetails = MessageUtils.getMessageDetails(
-                ComposeMessageActivity.this, cursor, msgItem.mMessageSize);
+                ComposeMessageActivity.this, cursor, messageSize);
         new AlertDialog.Builder(ComposeMessageActivity.this)
                 .setTitle(R.string.message_details_title)
                 .setMessage(messageDetails)
@@ -1881,6 +1892,11 @@ public class ComposeMessageActivity extends Activity
                     return true;
 
                 case MENU_FORWARD_MESSAGE:
+                    if (!isAllowForwardMessage(mMsgItem)) {
+                        Toast.makeText(ComposeMessageActivity.this,
+                                R.string.forward_size_over, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
                     forwardMessage(mMsgItem);
                     return true;
 
@@ -1969,6 +1985,19 @@ public class ComposeMessageActivity extends Activity
                     return false;
             }
         }
+    }
+
+    private boolean isAllowForwardMessage(MessageItem msgItem) {
+        int messageSize = msgItem.getSlideshow().getTotalMessageSize();
+        int forwardStrSize = getString(R.string.forward_prefix).getBytes().length;
+        int subjectSize =  (msgItem.mSubject == null) ? 0 : msgItem.mSubject.getBytes().length;
+        int totalSize = messageSize + forwardStrSize + subjectSize;
+        if (DEBUG) {
+            Log.e(TAG,"isAllowForwardMessage messageSize = "+ messageSize
+                    + ", forwardStrSize = "+forwardStrSize+ ", subjectSize = "+subjectSize
+                    + ", totalSize = " + totalSize);
+        }
+        return totalSize <= (MmsConfig.getMaxMessageSize() - SlideshowModel.SLIDESHOW_SLOP);
     }
 
     private void showCopySelectDialog(final MessageItem msgItem) {
@@ -3572,7 +3601,7 @@ public class ComposeMessageActivity extends Activity
                 menu.add(0, MENU_DELETE_THREAD, 0, R.string.delete_thread).setIcon(
                     android.R.drawable.ic_menu_delete);
                 if (FORWARD_CONVERSATION && mMsgListAdapter.hasSmsInConversation(cursor)) {
-                    menu.add(0, MENU_FOWARD_CONVERSATION, 0, R.string.menu_forward_conversation);
+                    menu.add(0, MENU_FORWARD_CONVERSATION, 0, R.string.menu_forward_conversation);
                 }
             }
         } else {
@@ -3668,7 +3697,7 @@ public class ComposeMessageActivity extends Activity
             case MENU_INSERT_SMILEY:
                 showSmileyDialog();
                 break;
-            case MENU_FOWARD_CONVERSATION: {
+            case MENU_FORWARD_CONVERSATION: {
                 Intent intent = new Intent(this, ManageMultiSelectAction.class);
                 intent.putExtra(MANAGE_MODE, MessageUtils.FORWARD_MODE);
                 intent.putExtra(THREAD_ID, mConversation.getThreadId());
@@ -4689,6 +4718,11 @@ public class ComposeMessageActivity extends Activity
         boolean showingAttachment = mAttachmentEditor.update(mWorkingMessage);
         mAttachmentEditorScrollView.setVisibility(showingAttachment ? View.VISIBLE : View.GONE);
         showSubjectEditor(showSubjectEditor || mWorkingMessage.hasSubject());
+        int subjectSize = mWorkingMessage.hasSubject()
+                ? mWorkingMessage.getSubject().toString().getBytes().length : 0;
+        if (mWorkingMessage.getSlideshow()!= null) {
+            mWorkingMessage.getSlideshow().setSubjectSize(subjectSize);
+        }
         if (mShowTwoButtons) {
             mAttachmentEditor.hideSlideshowSendButton();
         }
