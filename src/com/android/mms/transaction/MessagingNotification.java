CyanogenMod/android_packages_apps_Mms-caf

commit 1057979d3ba8ba80a9af9868410fb819a1236fab

Fix to return to previous activity when a sms/mms notification is selected.
When an MMS/SMS is received, the TaskStack is rebuilt
when displaying the notification.
Do not alter the TaskStack.

Change-Id: I8302ab5a4627f423ba787a327ee757165a4f0e83
CRs-Fixed: 580341

src/com/android/mms/transaction/MessagingNotification.java
@@ -983,7 +983,6 @@ public class MessagingNotification {
         if (isNew) {
             noti.setTicker(mostRecentNotification.mTicker);
         }
-        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -998,6 +997,7 @@ public class MessagingNotification {
         final Resources res = context.getResources();
         String title = null;
         Bitmap avatar = null;
+        PendingIntent pendingIntent = null;
         if (uniqueThreadCount > 1) {    // messages from multiple threads
             Intent mainActivityIntent = new Intent(Intent.ACTION_MAIN);
 
@@ -1006,7 +1006,8 @@ public class MessagingNotification {
                     | Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
             mainActivityIntent.setType("vnd.android-dir/mms-sms");
-            taskStackBuilder.addNextIntent(mainActivityIntent);
+            pendingIntent = PendingIntent.getActivity(context, 0,
+                    mainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             title = context.getString(R.string.message_count_notification, messageCount);
         } else {    // same thread, single or multiple messages
             title = mostRecentNotification.mTitle;
@@ -1032,8 +1033,9 @@ public class MessagingNotification {
                 }
             }
 
-            taskStackBuilder.addParentStack(ComposeMessageActivity.class);
-            taskStackBuilder.addNextIntent(mostRecentNotification.mClickIntent);
+            pendingIntent = PendingIntent.getActivity(context, 0,
+                    mostRecentNotification.mClickIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
         }
         // Always have to set the small icon or the notification is ignored
         noti.setSmallIcon(R.drawable.stat_notify_sms);
@@ -1043,8 +1045,7 @@ public class MessagingNotification {
 
         // Update the notification.
         noti.setContentTitle(title)
-            .setContentIntent(
-                    taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT))
+            .setContentIntent(pendingIntent)
             .addKind(Notification.KIND_MESSAGE)
             .setPriority(Notification.PRIORITY_DEFAULT);     // TODO: set based on contact coming
                                                              // from a favorite.

miscdec/caf-Mms

commit a98da5cfc9c1907d8834f0757c70ee9a552b7014

Mms: Create a fresh task stack to start ComposeMessageActivity
It is caused by 1057979d3ba8ba80a9af9868410fb819a1236fab commit.
When clicking received message via status bar, and enter into
ComposeMessageActivity, it didn't load messages, so no message
display in thread.

Revert the 1057979d3ba8ba80a9af9868410fb819a1236fab commit,
it will create a fresh task stack to start ComposeMessageActivity,
so it can call onStart() to load messages.

CRs-Fixed: 684667

Change-Id: Id7a76e0d2ea6803ebb92533b20cbe10b3e855926

src/com/android/mms/transaction/MessagingNotification.java
@@ -997,6 +997,7 @@ public class MessagingNotification {
         if (isNew) {
             noti.setTicker(mostRecentNotification.mTicker);
         }
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -1011,7 +1012,6 @@ public class MessagingNotification {
         final Resources res = context.getResources();
         String title = null;
         Bitmap avatar = null;
-        PendingIntent pendingIntent = null;
         if (uniqueThreadCount > 1) {    // messages from multiple threads
             Intent mainActivityIntent = new Intent(Intent.ACTION_MAIN);
 
@@ -1020,8 +1020,7 @@ public class MessagingNotification {
                     | Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
             mainActivityIntent.setType("vnd.android-dir/mms-sms");
-            pendingIntent = PendingIntent.getActivity(context, 0,
-                    mainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            taskStackBuilder.addNextIntent(mainActivityIntent);
             title = context.getString(R.string.message_count_notification, messageCount);
         } else {    // same thread, single or multiple messages
             title = mostRecentNotification.mTitle;
@@ -1047,9 +1046,8 @@ public class MessagingNotification {
                 }
             }
 
-            pendingIntent = PendingIntent.getActivity(context, 0,
-                    mostRecentNotification.mClickIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
+            taskStackBuilder.addParentStack(ComposeMessageActivity.class);
+            taskStackBuilder.addNextIntent(mostRecentNotification.mClickIntent);
         }
         // Always have to set the small icon or the notification is ignored
         noti.setSmallIcon(R.drawable.stat_notify_sms);
@@ -1059,9 +1057,10 @@ public class MessagingNotification {
 
         // Update the notification.
         noti.setContentTitle(title)
-            .setContentIntent(pendingIntent)
-            .addKind(Notification.KIND_MESSAGE)
-            .setPriority(Notification.PRIORITY_DEFAULT);     // TODO: set based on contact coming
+                .setContentIntent(
+                        taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT))
+                .addKind(Notification.KIND_MESSAGE)
+                .setPriority(Notification.PRIORITY_DEFAULT);     // TODO: set based on contact coming
                                                              // from a favorite.
 
         int defaults = 0;