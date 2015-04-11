3d0bd4d042a727816edaf36a96144cb30770a2ec
weichengzheng <zhengwc@pci-suntektech.com>
2015-04-11 17:25:06 +0800

    Mms: Attachment lost when send MMS to email address.
    - When send MMS to email or add subject, it run RCS process.
    - Add send MMS condition when selecting attachment.
    CRs-Fixed: 819539 819413 819407

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3946,7 +3946,8 @@ public class ComposeMessageActivity extends Activity
                 }
                 if (sizeLimit > 0) {
                     MessageUtils.recordVideo(this,
-                        getMakRequestCode(replace, REQUEST_CODE_TAKE_VIDEO), sizeLimit);
+                        getMakRequestCode(replace, REQUEST_CODE_TAKE_VIDEO), sizeLimit,
+                        mWorkingMessage.requiresMms());
                 } else {
                     Toast.makeText(this,
                             getString(R.string.message_too_big_for_video),
@@ -3963,7 +3964,8 @@ public class ComposeMessageActivity extends Activity
             case AttachmentPagerAdapter.RECORD_SOUND:
                 long sizeLimit = computeAttachmentSizeLimit(slideShow, currentSlideSize);
                 MessageUtils.recordSound(this,
-                        getMakRequestCode(replace, REQUEST_CODE_RECORD_SOUND), sizeLimit);
+                        getMakRequestCode(replace, REQUEST_CODE_RECORD_SOUND), sizeLimit,
+                        mWorkingMessage.requiresMms());
                 break;
 
             case AttachmentPagerAdapter.ADD_SLIDESHOW:
@@ -3999,6 +4001,10 @@ public class ComposeMessageActivity extends Activity
                 }
                 break;
             case AttachmentPagerAdapter.ADD_MAP:
+                 if (mWorkingMessage.requiresMms()) {
+                     toast(R.string.rcs_service_is_not_available);
+                     break;
+                 }
                 try {
                     Intent intent = new Intent();
                     intent.setAction("com.suntek.mway.rcs.MAP_POSITION_SELECT");
@@ -4264,7 +4270,8 @@ public class ComposeMessageActivity extends Activity
                 || (requestCode == REQUEST_CODE_VCARD_GROUP)
                 || (requestCode == REQUEST_CODE_SAIYUN)
                 || (requestCode == REQUEST_SELECT_LOCAL_AUDIO);
-        if (mIsRcsEnabled && mSupportApi.isOnline() && isRcsMessage 
+        boolean isMms = mWorkingMessage.requiresMms();
+        if (!isMms && mIsRcsEnabled && mSupportApi.isOnline() && isRcsMessage
                 && (mSendButtonMms != null && mSendButtonMms.getVisibility() == View.GONE)) {
             switch (requestCode) {
                 case PHOTO_CROP:
--- src/com/android/mms/ui/MessageUtils.java
+++ src/com/android/mms/ui/MessageUtils.java
@@ -824,14 +824,16 @@ public class MessageUtils {
         dialog.show();
     }
 
-    public static void recordSound(Activity activity, int requestCode, long sizeLimit) {
+    public static void recordSound(Activity activity, int requestCode, long sizeLimit,
+            boolean isMms) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType(ContentType.AUDIO_AMR);
         intent.setClassName("com.android.soundrecorder",
                 "com.android.soundrecorder.SoundRecorder");
         // add RCS recordSound time add size limit
-        if (RcsApiManager.getSupportApi().isOnline()) {
-            intent.putExtra(android.provider.MediaStore.Audio.Media.EXTRA_MAX_BYTES, sizeLimit*1024);
+        if (!isMms && RcsApiManager.getSupportApi().isOnline()) {
+            intent.putExtra(android.provider.MediaStore
+                    .Audio.Media.EXTRA_MAX_BYTES, sizeLimit*1024);
         } else {
             intent.putExtra(android.provider.MediaStore.Audio.Media.EXTRA_MAX_BYTES, sizeLimit);
         }
@@ -839,9 +841,10 @@ public class MessageUtils {
         activity.startActivityForResult(intent, requestCode);
     }
 
-    public static void recordVideo(Activity activity, int requestCode, long sizeLimit) {
+    public static void recordVideo(Activity activity, int requestCode,
+            long sizeLimit, boolean isMms) {
         // add RCS recordVideo time add size limit
-        if (RcsApiManager.getSupportApi().isOnline()) {
+        if (!isMms && RcsApiManager.getSupportApi().isOnline()) {
             Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
             intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 10.0);
             intent.putExtra("android.intent.extra.sizeLimit", sizeLimit*1024);
