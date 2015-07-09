b1e4d2e8d3779e926bb359fe18a5856aac5a77f7
wangjing <wangjing@codeaurora.org>
2015-07-09 12:56:21 +0800

    Mms: Fix threads doesn't refresh when resumed from pause status.
    When conversation list was in pause status, it removed the content
    changed listener to avoid refresh threads. During the pause status,
    if device received messages and then resumed conversation list, the
    conversation list did not query database again, it caused the new
    messages could not be shown in thread.
    
    Add query database operation in onResume(), if did not query database
    before.
    
    CRs-Fixed: 867317
    Change-Id: Ie2db01f728e8a82805b3b1c55ea27c352c8f530c

--- src/com/android/mms/ui/ConversationList.java
+++ src/com/android/mms/ui/ConversationList.java
@@ -547,6 +547,9 @@ public class ConversationList extends ListActivity implements DraftCache.OnDraft
         }
 
         mListAdapter.setOnContentChangedListener(mContentChangedListener);
+        if (!mDoOnceAfterFirstQuery) {
+            startAsyncQuery();
+        }
         mIsRunning = true;
     }
 
