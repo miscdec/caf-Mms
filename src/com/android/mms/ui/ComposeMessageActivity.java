a3193a467dfedaa474d8e60c766d7923df0b7910
wangjing <wangjing@codeaurora.org>
2015-07-15 13:45:41 +0800

    Mms: Fix the popup menu show wrong option in compose page action mode
    It should update the popup menu after message item checked state changed.
    But now there is not this action, so the option always shows "select all".
    
    Try to update the popup menu after message item checked state changed.
    
    Change-Id: I97222f8240ee8e9fd448c5053975f1a0df5c7175
    CRs-Fixed: 871509

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -7802,10 +7802,8 @@ public class ComposeMessageActivity extends Activity
                         @Override
                         public boolean onPopupItemClick(int itemId) {
                             if (itemId == RcsSelectionMenu.SELECT_OR_DESELECT) {
-                                boolean selectAll = getListView().getCheckedItemCount() <
-                                        getListView().getCount() ? true : false;
-                                checkAll(selectAll);
-                                mSelectionMenu.updateSelectAllMode(selectAll);
+                                checkAll(!allItemsSelected());
+                                mSelectionMenu.updateSelectAllMode(allItemsSelected());
                             }
                             return true;
                         }
@@ -8544,6 +8542,8 @@ public class ComposeMessageActivity extends Activity
 
             mode.getMenu().findItem(R.id.selection_toggle).setTitle(getString(
                     allItemsSelected() ? R.string.deselected_all : R.string.selected_all));
+            mSelectionMenu.setTitle(getString(R.string.selected_count, mCheckedCount));
+            mSelectionMenu.updateSelectAllMode(allItemsSelected());
         }
 
         private boolean allItemsSelected() {
