7bc66c5e7c5b67209b20e8776f06aadce08ea5fa
pengfeix <pengfeix@codeaurora.org>
2015-08-11 11:55:34 +0800

    Mms: Delete invalid prompts when don't select any audio in Mms.
     - Delete invalid prompts.
    
    Change-Id: I8386a5cd62ce08ad43768c94f5ebb1c7cce81e80
    CRs-Fixed: 885192

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -4655,8 +4655,6 @@ public class ComposeMessageActivity extends Activity
                 if (uri == null) {
                     uri = data.getData();
                 } else if (Settings.System.DEFAULT_RINGTONE_URI.equals(uri)) {
-                    Toast.makeText(ComposeMessageActivity.this,
-                            getString(R.string.file_suffix_vaild_tip), 0).show();
                     break;
                 }
                 addAudio(uri, append);
