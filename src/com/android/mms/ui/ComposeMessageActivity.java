3ac211adb54fa502c4d545b2c55a8f0496ac4eb5
zhibinw <zhibin@codeaurora.org>
2016-12-16 12:50:26 +0800

    Fix Mms force close when onNewIntent without permissions.
    This issue is test with script, when send multiple intents to
    compose quickly on first launch, it may not finish to start
    permission guard activity and finish self, so onNewIntent will
    be called without permission.
    
    Return from onNewIntent if permission is not granted.
    
    Change-Id: Ife8a4d2b0054a254e47a0e6332138c4549312af8
    CRs-Fixed: 1101638

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2779,6 +2779,11 @@ public class ComposeMessageActivity extends Activity
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        if (!(MessageUtils.hasBasicPermissions() && MessageUtils
+                .hasPermissions(MessageUtils.sSMSExtendPermissions)))
+        {
+            return;
+        }
 
         setIntent(intent);
 
